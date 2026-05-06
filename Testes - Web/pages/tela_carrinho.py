9987a84e4e31c1313eb888fcf3773be20aa1c113
joaogabrielsilvarabelo <b5rnt.1c3@gmail.com>
2026-05-06 19:53:49 -0300

    Aumentei os tempos de espera e mudei a espera do ir para checkout para esperar o elemento ser clicável

--- Testes - Web/pages/tela_carrinho.py
+++ Testes - Web/pages/tela_carrinho.py
@@ -1,7 +1,6 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
 class CartPage:
     def __init__(self, driver):
@@ -9,29 +8,28 @@ class CartPage:
         self.carrinho_icone = (By.CLASS_NAME, "shopping_cart_link")
         self.botao_checkout = (By.ID, "checkout")
         self.cart_badge = (By.CLASS_NAME, "shopping_cart_badge")
-        self.cart_list = (By.CLASS_NAME, "cart_list")
+        self.cart_item = (By.CLASS_NAME, "cart_item")
 
     def adicionar_produto(self, produto_id):
-        WebDriverWait(self.driver, 10).until(
+        WebDriverWait(self.driver, 15).until(
             EC.element_to_be_clickable((By.ID, f"add-to-cart-{produto_id}"))
         ).click()
-        WebDriverWait(self.driver, 10).until(
+        WebDriverWait(self.driver, 15).until(
             EC.visibility_of_element_located(self.cart_badge)
         )
 
     def abrir_carrinho(self):
-        WebDriverWait(self.driver, 10).until(
+        WebDriverWait(self.driver, 15).until(
             EC.element_to_be_clickable(self.carrinho_icone)
         ).click()
-        WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located(self.cart_list)
+        WebDriverWait(self.driver, 15).until(
+            EC.visibility_of_element_located(self.cart_item)
         )
 
     def ir_para_checkout(self):
-        WebDriverWait(self.driver, 10).until(
+        WebDriverWait(self.driver, 15).until(
             EC.element_to_be_clickable(self.botao_checkout)
         ).click()
-        time.sleep(2)
         WebDriverWait(self.driver, 15).until(
             EC.url_contains("checkout-step-one")
         )

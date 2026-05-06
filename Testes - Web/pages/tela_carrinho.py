f31b1b473874932e92c7a6f7a259053bf7b61af5
joaogabrielsilvarabelo <b5rnt.1c3@gmail.com>
2026-05-06 20:27:31 -0300

    Mudei para esperar a url mudar

--- Testes - Web/pages/tela_carrinho.py
+++ Testes - Web/pages/tela_carrinho.py
@@ -8,28 +8,27 @@ class CartPage:
         self.carrinho_icone = (By.CLASS_NAME, "shopping_cart_link")
         self.botao_checkout = (By.ID, "checkout")
         self.cart_badge = (By.CLASS_NAME, "shopping_cart_badge")
-        self.cart_item = (By.CLASS_NAME, "cart_item")
 
     def adicionar_produto(self, produto_id):
-        WebDriverWait(self.driver, 40).until(
+        WebDriverWait(self.driver, 30).until(
             EC.element_to_be_clickable((By.ID, f"add-to-cart-{produto_id}"))
         ).click()
-        WebDriverWait(self.driver, 40).until(
+        WebDriverWait(self.driver, 30).until(
             EC.visibility_of_element_located(self.cart_badge)
         )
 
     def abrir_carrinho(self):
-        WebDriverWait(self.driver, 40).until(
+        WebDriverWait(self.driver, 30).until(
             EC.element_to_be_clickable(self.carrinho_icone)
         ).click()
-        WebDriverWait(self.driver, 40).until(
-            EC.visibility_of_element_located(self.cart_item)
+        WebDriverWait(self.driver, 30).until(
+            EC.url_contains("cart")
         )
 
     def ir_para_checkout(self):
-        WebDriverWait(self.driver, 40).until(
+        WebDriverWait(self.driver, 30).until(
             EC.element_to_be_clickable(self.botao_checkout)
         ).click()
-        WebDriverWait(self.driver, 40).until(
+        WebDriverWait(self.driver, 30).until(
             EC.url_contains("checkout-step-one")
         )

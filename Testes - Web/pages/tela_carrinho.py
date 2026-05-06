ae2859886fe55fa5b093726a315c1eb17c09516a
joaogabrielsilvarabelo <b5rnt.1c3@gmail.com>
2026-05-06 19:16:09 -0300

    Mudei o carrinho icone e o adicionar produto para esperarem badge aparecer para garantir que estejam carregados

--- Testes - Web/pages/tela_carrinho.py
+++ Testes - Web/pages/tela_carrinho.py
@@ -5,13 +5,17 @@ from selenium.webdriver.support import expected_conditions as EC
 class CartPage:
     def __init__(self, driver):
         self.driver = driver
-        self.carrinho_icone = (By.ID, "shopping_cart_container")
+        self.carrinho_icone = (By.CLASS_NAME, "shopping_cart_link")
         self.botao_checkout = (By.ID, "checkout")
+        self.cart_badge = (By.CLASS_NAME, "shopping_cart_badge")
 
     def adicionar_produto(self, produto_id):
         WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.ID, f"add-to-cart-{produto_id}"))
         ).click()
+        WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.cart_badge)
+        )
 
     def abrir_carrinho(self):
         WebDriverWait(self.driver, 10).until(

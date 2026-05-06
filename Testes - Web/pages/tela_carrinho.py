44555b83374be6a3516e12eed1664848eb23e189
joaogabrielsilvarabelo <b5rnt.1c3@gmail.com>
2026-05-06 13:30:26 -0300

    Corrigi o nome da classe do tela carrinho

--- Testes - Web/pages/tela_carrinho.py
+++ Testes - Web/pages/tela_carrinho.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-class CarrinhoPage:
+class CartPage:
     def __init__(self, driver):
         self.driver = driver
         self.carrinho_icone = (By.ID, "shopping_cart_container")

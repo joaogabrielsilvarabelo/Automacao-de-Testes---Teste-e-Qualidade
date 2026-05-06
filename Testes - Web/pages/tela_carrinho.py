d9a08732ddc00d0fed90ed46f9db11f707f1bcdd
joaogabrielsilvarabelo <b5rnt.1c3@gmail.com>
2026-05-06 19:36:30 -0300

    Reverti o arquivo para como estava antes quando deu certo

--- Testes - Web/pages/tela_carrinho.py
+++ Testes - Web/pages/tela_carrinho.py
@@ -1,7 +1,6 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
 class CartPage:
     def __init__(self, driver):
@@ -30,7 +29,9 @@ class CartPage:
         WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable(self.botao_checkout)
         ).click()
+        import time
         time.sleep(2)
+        print(f"\nURL após clicar checkout: {self.driver.current_url}")
         WebDriverWait(self.driver, 15).until(
             EC.url_contains("checkout-step-one")
         )

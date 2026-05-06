007bfdde7c33b2e24224e7a31f75ac11fe34ab4d
joaogabrielsilvarabelo <b5rnt.1c3@gmail.com>
2026-05-06 20:30:02 -0300

    Aumentei mais tempo de espera

--- Testes - Web/pages/tela_carrinho.py
+++ Testes - Web/pages/tela_carrinho.py
@@ -10,25 +10,25 @@ class CartPage:
         self.cart_badge = (By.CLASS_NAME, "shopping_cart_badge")
 
     def adicionar_produto(self, produto_id):
-        WebDriverWait(self.driver, 30).until(
+        WebDriverWait(self.driver, 40).until(
             EC.element_to_be_clickable((By.ID, f"add-to-cart-{produto_id}"))
         ).click()
-        WebDriverWait(self.driver, 30).until(
+        WebDriverWait(self.driver, 40).until(
             EC.visibility_of_element_located(self.cart_badge)
         )
 
     def abrir_carrinho(self):
-        WebDriverWait(self.driver, 30).until(
+        WebDriverWait(self.driver, 40).until(
             EC.element_to_be_clickable(self.carrinho_icone)
         ).click()
-        WebDriverWait(self.driver, 30).until(
+        WebDriverWait(self.driver, 40).until(
             EC.url_contains("cart")
         )
 
     def ir_para_checkout(self):
-        WebDriverWait(self.driver, 30).until(
+        WebDriverWait(self.driver, 40).until(
             EC.element_to_be_clickable(self.botao_checkout)
         ).click()
-        WebDriverWait(self.driver, 30).until(
+        WebDriverWait(self.driver, 40).until(
             EC.url_contains("checkout-step-one")
         )
